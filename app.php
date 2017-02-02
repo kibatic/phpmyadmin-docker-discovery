6bcadf827d050e26411c53f13041ccd7f358b754
jcrombez <jeremy.crombez@kitpages.fr>
2017-02-02 15:56:37 +0100

    init

---
+++ app.php
@@ -0,0 +1,37 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Docker\Docker;
+
+$docker = new Docker();
+
+$containers = $docker->getContainerManager()->findAll();
+$validImageNames = ['mariadb:.+', 'mysql:.+'];
+$cfg = [];
+
+foreach ($containers as $container) {
+    foreach ($validImageNames as $validImageName) {
+        if (preg_match('/' . $validImageName . '/', $container->getImage())) {
+            break;
+        }
+
+        continue 2;
+    }
+
+    $containerName = str_replace('/', '', $container->getNames()[0]);
+
+    $network = (array) $container->getNetworkSettings()->getNetworks();
+    $ipAddress = array_shift($network)->getIpAddress();
+
+    $cfg['Servers'][] = [
+        'auth_type' => 'cookie',
+        'host' => $ipAddress,
+        'verbose' => $containerName . ' (' . $ipAddress . ')',
+        'connect_type' => 'tcp',
+        'compress' => false,
+        'AllowNoPassword' => false
+    ];
+}
+
+var_dump($cfg);
