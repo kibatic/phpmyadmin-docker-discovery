653a566edea0bba64db9703a91a66cb81b232284
jcrombez <jeremy.crombez@kibatic.com>
2017-07-27 14:37:36 +0200

    adapt for new docker-discovery api

--- app.php
+++ app.php
@@ -4,18 +4,23 @@ require __DIR__ . '/vendor/autoload.php';
 
 $dockerDiscovery = new \Kibatic\DockerDiscovery\DockerDiscovery();
 
-$containers = $dockerDiscovery->discover(['mariadb:.+', 'mysql:.+']);
+$containers = $dockerDiscovery->discover(
+    \Kibatic\DockerDiscovery\DockerDiscovery::FILTER_IMAGE,
+    ['mariadb:.+', 'mysql:.+']
+);
 
 $cfg = [];
 $i = 1;
 
 foreach ($containers as $container) {
     $cfg['Servers'][$i] = [
-        'host' => $container['ip'],
-        'verbose' => $container['name'],
+        'host' => $container->ipAddresses[0],
+        'verbose' => $container->name,
         'connect_type' => 'tcp',
         'compress' => false
     ];
 
     $i++;
 }
+
+var_dump($cfg);
